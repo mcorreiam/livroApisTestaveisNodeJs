a448071a5aca5e2d358139c341466bed5bb5d4b3
mcorreiam <m.correia.m@gmail.com>
2020-06-17 14:30:24 -0300

    TDD red state

---
+++ test/integration/routes/products_spec.js
@@ -0,0 +1,17 @@
+describe('Routes: Products', () => {
+    const defaultProduct = {
+        name: 'Default product',
+        description: 'product description',
+        price: 100
+    };
+
+    describe('GET /products', () => {
+        it('should return a list of products', done => {
+            request.get('/products')
+            .end((err, res) => {
+                expect(res.body[0]).to.eql(defaultProduct);
+                done(err);
+            })
+        })
+    })
+})
